fec5e7f56ae7ca06eca82843a0cada529170c91d
parimalahire2304-py <parimalahire91@gmail.com>
2026-04-17 20:20:29 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/HospitalPatientRecordSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPatientRecordSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalPatientRecordSystemApplication.class, args);
+	}
+
+}
